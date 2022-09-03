4b3b13534b228567a16821c359b8858d7784aa33
Luke Wong <lukewong93@gmail.com>
2022-09-03 14:57:24 +0800

    Update App.js
    Import App bar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import Post from "./pages/Post";
+import PrimarySearchAppBar from "./components/AppBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
