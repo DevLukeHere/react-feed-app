029cac7f4959774ae78b1896a50b225f081b171b
Luke Wong <lukewong93@gmail.com>
2022-09-03 19:00:37 +0800

    Update Post.js
    Fetch individual post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,10 +1,34 @@
 import { Container, Typography, Grid } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PostDetails from "../components/PostDetails";
+import { useParams } from "react-router-dom";
+const axios = require("axios");
 
 export default function Post() {
+  const { slug } = useParams();
+  const [post, setPost] = useState({})
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
-    //
+    setLoading(true);
+    axios
+      .get(`https://api.realworld.io/api/articles/${slug}`)
+      .then(function (response) {
+        // handle success
+        const post = response.data.article
+        setPost(post)
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error:", error);
+        setError(error)
+      })
+      .then(function () {
+        // always executed
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -17,7 +41,7 @@ export default function Post() {
             </Typography>
           </Grid>
           <Grid item>
-            <PostDetails />
+            <PostDetails post={post} loading={loading} />
           </Grid>
         </Grid>
       </Container>
