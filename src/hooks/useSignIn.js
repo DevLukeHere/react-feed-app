37b3f55ce6b76932563dc57efd47af7d1afc039f
Luke Wong <lukewong93@gmail.com>
2022-09-04 15:15:39 +0800

    Create useSignIn.js

---
+++ src/hooks/useSignIn.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+const axios = require("axios");
+
+export const useSignIn = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const signIn = async (email, password) => {
+    setLoading(true);
+    setError("");
+
+    axios
+      .post(`https://api.realworld.io/api/users/login`, {
+        user: {
+          email: email,
+          password: password,
+        },
+      })
+      .then(function (response) {
+        // handle success
+        const user = response.data.user;
+
+        localStorage.setItem("user", JSON.stringify(user));
+        dispatch({ type: "LOGIN", payload: user });
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        setError(error);
+        setLoading(false);
+      })
+      .then(function () {
+        // always executed
+        setLoading(false);
+      });
+  };
+
+  return { signIn, loading, error };
+};
